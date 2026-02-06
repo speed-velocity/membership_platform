50ece860d4e443b20e7dca9de41b4c10fcea7a1b
speed-velocity <shubhankarpandit21@gmail.com>
2026-02-07 02:30:41 +0530

    Add action posters for weekly recommendations

--- server/recommendationsSeed.js
+++ server/recommendationsSeed.js
@@ -1,19 +1,19 @@
 const ACTION_RECOMMENDATIONS = [
-  { title: 'Raid Redemption', kind: 'Movie' },
-  { title: 'Oldboy', kind: 'Movie' },
-  { title: 'Marco', kind: 'Movie' },
-  { title: 'HIT-The third case', kind: 'Movie' },
-  { title: 'Dhurandhar', kind: 'Movie' },
-  { title: 'WAR', kind: 'Movie' },
-  { title: 'Fight club', kind: 'Movie' },
-  { title: 'Rocky Handsome', kind: 'Movie' },
-  { title: 'Tenet', kind: 'Movie' },
-  { title: 'Extraction', kind: 'Movie' },
-  { title: 'Banshee', kind: 'Series' },
-  { title: 'Paatal lok', kind: 'Series' },
-  { title: 'The Terminal List', kind: 'Series' },
-  { title: 'Spartacus', kind: 'Series' },
-  { title: 'Rana Raidu', kind: 'Series' },
+  { title: 'Raid Redemption', kind: 'Movie', posterPath: 'posters/action/raid-redemption.jpg' },
+  { title: 'Oldboy', kind: 'Movie', posterPath: 'posters/action/oldboy.jpg' },
+  { title: 'Marco', kind: 'Movie', posterPath: 'posters/action/marco.jpg' },
+  { title: 'HIT-The third case', kind: 'Movie', posterPath: 'posters/action/hit-the-third-case.jpg' },
+  { title: 'Dhurandhar', kind: 'Movie', posterPath: 'posters/action/dhurandhar.jpg' },
+  { title: 'WAR', kind: 'Movie', posterPath: 'posters/action/war.jpg' },
+  { title: 'Fight club', kind: 'Movie', posterPath: 'posters/action/fight-club.jpg' },
+  { title: 'Rocky Handsome', kind: 'Movie', posterPath: 'posters/action/rocky-handsome.jpg' },
+  { title: 'Tenet', kind: 'Movie', posterPath: 'posters/action/tenet.jpg' },
+  { title: 'Extraction', kind: 'Movie', posterPath: 'posters/action/extraction.jpg' },
+  { title: 'Banshee', kind: 'Series', posterPath: 'posters/action/banshee.jpg' },
+  { title: 'Paatal lok', kind: 'Series', posterPath: 'posters/action/paatal-lok.jpg' },
+  { title: 'The Terminal List', kind: 'Series', posterPath: 'posters/action/the-terminal-list.jpg' },
+  { title: 'Spartacus', kind: 'Series', posterPath: 'posters/action/spartacus.jpg' },
+  { title: 'Rana Raidu', kind: 'Series', posterPath: 'posters/action/rana-raidu.jpg' },
 ];
 
 const GENRE_RECOMMENDATIONS = [
@@ -24,10 +24,11 @@ async function seedRecommendations(db) {
   for (const group of GENRE_RECOMMENDATIONS) {
     for (const item of group.items) {
       await db.run(
-        `INSERT INTO weekly_recommendations (genre, title, kind)
-         VALUES ($1, $2, $3)
-         ON CONFLICT (genre, title, kind) DO NOTHING`,
-        [group.genre, item.title, item.kind]
+        `INSERT INTO weekly_recommendations (genre, title, kind, poster_path)
+         VALUES ($1, $2, $3, $4)
+         ON CONFLICT (genre, title, kind)
+         DO UPDATE SET poster_path = EXCLUDED.poster_path`,
+        [group.genre, item.title, item.kind, item.posterPath || null]
       );
     }
   }
